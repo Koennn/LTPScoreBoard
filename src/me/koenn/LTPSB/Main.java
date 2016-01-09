f8cbeef37ee2c8a81a5875c380b1ba65948542af
Koennn <koenwillemse1@gmail.com>
2016-01-09 16:03:37 +0100

    Things

--- src/me/koenn/LTPSB/Main.java
+++ src/me/koenn/LTPSB/Main.java
@@ -2,9 +2,11 @@ package me.koenn.LTPSB;
 
 import me.koenn.LTPFL.LTPFactionLevels;
 import me.koenn.LTPSB.listeners.Listeners;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.*;
 import ru.tehkode.permissions.PermissionUser;
@@ -15,20 +17,27 @@ import java.util.List;
 
 import static org.bukkit.ChatColor.translateAlternateColorCodes;
 
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin {
 
+    public static Economy econ = null;
+    private static Main instance;
     public HashMap<String, Objective> objective = new HashMap<>();
     public HashMap<String, Score> name = new HashMap<>();
     public HashMap<String, Scoreboard> board = new HashMap<>();
-
     public HashMap<String, Integer> rotate = new HashMap<>();
+    public HashMap<Player, Integer> kills = new HashMap<>();
+    public HashMap<Player, Integer> deaths = new HashMap<>();
 
     private static double round(double value, int precision) {
         int scale = (int) Math.pow(10, precision);
         return (double) Math.round(value * scale) / scale;
     }
 
-    public void log(String msg){
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public void log(String msg) {
         getLogger().info("[LTPScoreBoard] " + msg);
     }
 
@@ -36,28 +45,45 @@ public class Main extends JavaPlugin{
     @SuppressWarnings("deprecation")
     public void onEnable() {
         log("All credits for this plugin go to Koenn");
+        if (!(setupEconomy())) {
+            log("Disabled due to no Vault dependency found!");
+            Bukkit.getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        instance = this;
         new ConfigManager(this.getConfig(), this);
         Bukkit.getPluginManager().registerEvents(new Listeners(this), this);
         Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> Bukkit.getServer().getOnlinePlayers().forEach(this::refreshBoard), 1, 5 * 20);
         Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Lag(), 100L, 1L);
     }
 
-    public void refreshBoard(Player p){
+    private boolean setupEconomy() {
+        if (Bukkit.getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public void refreshBoard(Player p) {
         String pname = p.getName();
         if (rotate.get(pname) == null) {
             rotate.put(pname, 0);
         }
         PermissionUser user = PermissionsEx.getUser(p);
-        String rank = ChatColor.stripColor(translateAlternateColorCodes('&', user.getPrefix().replace("(", "").replace(")", "")));
-        if(user.getPrefix() == "") {
+        String rank = ChatColor.stripColor(translateAlternateColorCodes('&', user.getPrefix().replace("[", "").replace("]", "")));
+        if (user.getPrefix() == "") {
             rank = "Default";
         }
-        Integer tokens = me.koenn.ltp.Main.getInstance().tokens.get(p.getUniqueId());
         ScoreboardManager manager = Bukkit.getScoreboardManager();
         board.put(pname, manager.getNewScoreboard());
         objective.put(pname, board.get(pname).registerNewObjective("info", "dummy"));
         objective.get(pname).setDisplaySlot(DisplaySlot.SIDEBAR);
-        objective.get(pname).setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "LostTimePark");
+        objective.get(pname).setDisplayName(translateAlternateColorCodes('&', "&8> &6LostTimePark &8<"));
         name.clear();
         List<String> scoreboard;
         if (rotate.get(pname) == 0) {
@@ -67,18 +93,29 @@ public class Main extends JavaPlugin{
             scoreboard = ConfigManager.getInstance().getList("scoreboard2");
             rotate.put(pname, 0);
         }
-
+        String balance;
+        if (econ.getBalance(p) > 1000000000) {
+            balance = (round(econ.getBalance(p) / 1000000000, 1) + "b").replace(".", ",");
+        } else if (econ.getBalance(p) > 1000000) {
+            balance = (round(econ.getBalance(p) / 1000000, 1) + "m").replace(".", ",");
+        } else if (econ.getBalance(p) > 1000) {
+            balance = (round(econ.getBalance(p) / 1000, 1) + "k").replace(".", ",");
+        } else {
+            balance = String.valueOf(econ.getBalance(p));
+        }
         int line = scoreboard.size() - 1;
         for (String s : scoreboard) {
             s = s.replace("{onlinePlayers}", String.valueOf(Bukkit.getServer().getOnlinePlayers().size()));
             s = s.replace("{playerName}", p.getName());
             s = s.replace("{factionLevel}", String.valueOf(LTPFactionLevels.getFactionLevel(p.getUniqueId())));
             s = s.replace("{pexRank}", rank);
-            s = s.replace("{killCount}", "0");
-            s = s.replace("{stattrackTokens}", String.valueOf(tokens));
-            s = s.replace("{playerBalance}", "0");
+            s = s.replace("{killCount}", String.valueOf(kills.get(p)));
+            s = s.replace("{deathCount}", String.valueOf(deaths.get(p)));
+            s = s.replace("{voteCount}", "0");
+            s = s.replace("{playerBalance}", "$" + balance);
             s = s.replace("{tps}", String.valueOf(round(Lag.getTPS(), 1) + "/20.0"));
-            s = s.replace("?", "\u226B");
+            s = s.replace("?", "\u2726");
+            s = s.replace("™", "\u2122");
             s = translateAlternateColorCodes('&', s);
             name.put(pname, objective.get(pname).getScore(s));
             name.get(pname).setScore(line);
@@ -88,7 +125,7 @@ public class Main extends JavaPlugin{
     }
 
     @Override
-    public void onDisable(){
+    public void onDisable() {
         log("All credits for this plugin go to Koenn");
         ScoreboardManager manager = Bukkit.getScoreboardManager();
         for (Player p : Bukkit.getServer().getOnlinePlayers()) {
--- src/me/koenn/LTPSB/listeners/Listeners.java
+++ src/me/koenn/LTPSB/listeners/Listeners.java
@@ -1,14 +1,12 @@
 package me.koenn.LTPSB.listeners;
 
 import me.koenn.LTPSB.Main;
-import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class Listeners implements Listener{
+public class Listeners implements Listener {
 
     private Main main;
 
@@ -17,15 +15,15 @@ public class Listeners implements Listener{
     }
 
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            event.getPlayer().sendMessage(ChatColor.AQUA + "You clicked a " + ChatColor.BOLD + event.getClickedBlock().getType().toString().toLowerCase().replace("_", ""));
-        }
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        main.refreshBoard(e.getPlayer());
     }
 
     @EventHandler
-    @SuppressWarnings("deprecation")
-    public void onPlayerJoin(PlayerJoinEvent e) {
-        main.refreshBoard(e.getPlayer());
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        main.deaths.put(e.getEntity(), main.deaths.get(e.getEntity()));
+        if (e.getEntity().getKiller() != null) {
+            main.kills.put(e.getEntity().getKiller(), main.kills.get(e.getEntity().getKiller()));
+        }
     }
 }
